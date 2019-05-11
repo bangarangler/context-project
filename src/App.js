b350baf5212fc96aadb9228b64362f667401302d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-11 19:25:27 -0400

    Added Form with Material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar.js";
+import Form from './components/Form.js'
 
 class App extends Component {
   render() {
-    return <Navbar />;
+return (
+<>
+<Navbar />
+<Form />
+</>
+)
   }
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,10 @@ class Navbar extends Component {
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
-              <InputBase placeholder='Search...' classes={{root: classes.inputRoot, input: classes.inputInput}} />
+              <InputBase
+                placeholder="Search..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
             </div>
           </Toolbar>
         </AppBar>
