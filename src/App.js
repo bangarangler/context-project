57010b34d88543f63bf92614fc8f980df6a8ce78
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-11 19:46:23 -0400

    Added createContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar.js";
-import Form from './components/Form.js'
+import Form from "./components/Form.js";
+import PageContent from "./components/PageContent.js";
+import { ThemeProvider } from './contexts/ThemeContext.js'
 
 class App extends Component {
   render() {
-return (
-<>
-<Navbar />
-<Form />
-</>
-)
+    return (
+<ThemeProvider>
+      <PageContent>
+        <Navbar />
+        <Form />
+      </PageContent>
+    </ThemeProvider>
+    );
   }
 }
 
