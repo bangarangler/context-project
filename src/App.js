e1613abf88ea5705ec7792e3e352e5a1262535b9
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-12 08:50:48 -0400

    Added ThemeContext

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar.js";
 import Form from "./components/Form.js";
 import PageContent from "./components/PageContent.js";
-import { ThemeProvider } from './contexts/ThemeContext.js'
+import { ThemeProvider } from "./contexts/ThemeContext.js";
 
 class App extends Component {
   render() {
     return (
-<ThemeProvider>
-      <PageContent>
-        <Navbar />
-        <Form />
-      </PageContent>
-    </ThemeProvider>
+      <ThemeProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </ThemeProvider>
     );
   }
 }
